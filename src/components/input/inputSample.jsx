89351ed5da6995f4017ddf07634803e5d698447f
GyuHyunA <ghahn97@gmail.com>
2023-12-20 16:50:27 +0900

    여러개 input 관리

--- src/components/input/inputSample.jsx
+++ src/components/input/inputSample.jsx
@@ -1,16 +1,33 @@
 import React, { useState } from "react";
 
 const InputSample = () => {
-  const [text, setText] = useState("");
+  const [input, setInput] = useState({
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = input;
+
   const onChange = (e) => {
-    setText(e.target.value);
+    const { value, name } = e.target;
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+  const onReset = () => {
+    setInput({
+      name: "",
+      nickname: "",
+    });
   };
   return (
     <>
-      <input type="text" onChange={onChange}/>
-      <button>초기화</button>
+      <input name="name" type="text" placeholder="이름" onChange={onChange} />
+      <input name="nickname" type="text" placeholder="닉네임" onChange={onChange} />
+      <button onClick={onReset}>검색</button>
       <div>
-        <b>값 : {text} </b>
+        <b>값 : </b>
+        <b>{name}({nickname})</b>
       </div>
     </>
   );
