af457864ecdc0399623a2bb102973eaea7e02da1
GyuHyunA <ghahn97@gmail.com>
2023-12-29 11:15:20 +0900

    배열 추가 및 제거

--- src/components/Basic/array/UserList.jsx
+++ src/components/Basic/array/UserList.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
-const users = [
+const userset = [
   {
     id: 1,
     username: "Lee",
@@ -18,23 +18,78 @@ const users = [
   },
 ];
 
-const User = ({ user }) => {
-  return (
-    <div>
-      <b>{user.username}</b>
-      <span>({user.email})</span>
-    </div>
-  );
-};
-
+// App 부분
 const UserList = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+  const { username, email } = inputs;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const [users, setUsers] = useState(userset);
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers([...users, user]);
+    setInputs({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
+  };
+
+  const onRemove = (id) => {
+    setUsers(users.filter((u) => u.id !== id));
+  };
   return (
     <>
-      {users.map((v) => (
-        <User key={v.id} user={v} />
-      ))}
+      <CreateUser username={username} email={email} onCreate={onCreate} onChange={onChange} />
+      <UserListPap users={users} onRemove={onRemove} />
     </>
   );
 };
 
 export default UserList;
+
+// UserLsit Section
+const User = ({ user, onRemove }) => {
+  return (
+    <div>
+      <b>{user.username}</b>
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
+    </div>
+  );
+};
+
+const UserListPap = ({ users, onRemove }) => {
+  return (
+    <>
+      {users.map((v) => (
+        <User key={v.id} user={v} onRemove={onRemove} />
+      ))}
+    </>
+  );
+};
+
+// CreateUser부분
+const CreateUser = ({ username, email, onChange, onCreate }) => {
+  return (
+    <>
+      <input type="text" name="username" placeholder="계정명" onChange={onChange} value={username} />
+      <input type="text" name="email" placeholder="이메일" onChange={onChange} value={email} />
+      <button onClick={onCreate}>등록</button>
+    </>
+  );
+};
--- src/components/Basic/input/inputSample.jsx
+++ src/components/Basic/input/inputSample.jsx
@@ -2,14 +2,12 @@ import React, { useState } from "react";
 import { useRef } from "react";
 
 const InputSample = () => {
-  const nameInput = useRef()
+  const nameInput = useRef();
   const [input, setInput] = useState({
     name: "",
     nickname: "",
   });
   const { name, nickname } = input;
-  
-
 
   const onChange = (e) => {
     const { value, name } = e.target;
@@ -23,16 +21,18 @@ const InputSample = () => {
       name: "",
       nickname: "",
     });
-  nameInput.current.focus()
+    nameInput.current.focus();
   };
   return (
     <>
-      <input name="name" type="text" placeholder="이름" onChange={onChange} ref={nameInput}/>
+      <input name="name" type="text" placeholder="이름" onChange={onChange} ref={nameInput} />
       <input name="nickname" type="text" placeholder="닉네임" onChange={onChange} />
       <button onClick={onReset}>검색</button>
       <div>
         <b>값 : </b>
-        <b>{name}({nickname})</b>
+        <b>
+          {name}({nickname})
+        </b>
       </div>
     </>
   );
