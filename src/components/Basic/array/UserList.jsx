37a77bcd7882255ba8e0563b83f9fa6a22bc4190
GyuHyunA <ghahn97@gmail.com>
2023-12-30 13:24:23 +0900

    배열 수정

--- src/components/Basic/array/UserList.jsx
+++ src/components/Basic/array/UserList.jsx
@@ -5,16 +5,19 @@ const userset = [
     id: 1,
     username: "Lee",
     email: "Lee@gmail.com",
+    active: true,
   },
   {
     id: 2,
     username: "Ahn",
     email: "Ahn@gmail.com",
+    active: false,
   },
   {
     id: 3,
     username: "Hong",
     email: "Hong@gmail.com",
+    active: false,
   },
 ];
 
@@ -33,6 +36,7 @@ const UserList = () => {
       [name]: value,
     });
   };
+  // userSet 관리
   const [users, setUsers] = useState(userset);
   const nextId = useRef(4);
   const onCreate = () => {
@@ -41,7 +45,7 @@ const UserList = () => {
       username,
       email,
     };
-    setUsers([...users, user]);
+    setUsers(users.concat(user));
     setInputs({
       username: "",
       email: "",
@@ -52,10 +56,13 @@ const UserList = () => {
   const onRemove = (id) => {
     setUsers(users.filter((u) => u.id !== id));
   };
+  const onToggle = (id) => {
+    setUsers(users.map((user) => (user.id === id ? { ...user, active: !user.active } : user)))
+  };
   return (
     <>
       <CreateUser username={username} email={email} onCreate={onCreate} onChange={onChange} />
-      <UserListPap users={users} onRemove={onRemove} />
+      <UserListPap users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
   );
 };
@@ -63,21 +70,23 @@ const UserList = () => {
 export default UserList;
 
 // UserLsit Section
-const User = ({ user, onRemove }) => {
+const User = ({ user, onRemove, onToggle }) => {
   return (
     <div>
-      <b>{user.username}</b>
+      <b style={{ cursor: "pointer", color: user.active ? "green" : "black" }} onClick={() => onToggle(user.id)}>
+        {user.username}
+      </b>
       <span>({user.email})</span>
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 };
 
-const UserListPap = ({ users, onRemove }) => {
+const UserListPap = ({ users, onRemove, onToggle }) => {
   return (
     <>
       {users.map((v) => (
-        <User key={v.id} user={v} onRemove={onRemove} />
+        <User key={v.id} user={v} onRemove={onRemove} onToggle={onToggle} />
       ))}
     </>
   );
